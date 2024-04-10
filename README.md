a593e62e908f8df9e6fdc4b1ae543fe13e4f7e66
Paulina Becker <paulina-becker@hotmail.com>
2024-04-10 16:41:54 -0400

    Always music

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Always-music
+
+Descripción
+La escuela de música Always Music es reconocida en la ciudad por graduar a grandes
+músicos de reconocimiento mundial, sin embargo, a pesar de lo mucho que ha crecido con
+el tiempo, nunca dejaron de utilizar excel como base de datos y han decidido cambiar esto
+por un desarrollo personalizado.
+En este desafío deberás desarrollar una aplicación en Node que realice consultas a
+PostgreSQL con el paquete “pg” para:
+● Agregar un nuevo estudiante.
+● Consultar los estudiantes registrados.
+● Consultar estudiante por rut.
+● Actualizar la información de un estudiante.
+● Eliminar el registro de un estudiante.
+Ya que el caso se trata de un proceso de desarrollo, la interacción la debes realizar con
+argumentos por la línea de comandos.
+Antes de iniciar este desafío deberás crear una base de datos y una tabla con las siguientes
+columnas:
+● Nombre
+● Rut
+● Curso
+● Nivel
+
+Requerimientos
+1. Crear una función asíncrona para registrar un nuevo estudiante en la base de datos.
+
+2. Crear una función asíncrona para obtener por consola el registro de un estudiante
+por medio de su rut. 
+3. Crear una función asíncrona para obtener por consola todos los estudiantes
+registrados. 
+4. Crear una función asíncrona para actualizar los datos de un estudiante en la base de
+datos. 
+5. Crear una función asíncrona para eliminar el registro de un estudiante de la base de
+datos.
---
+++ app.js
@@ -0,0 +1,92 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const config = {
+  user: "postgres",
+  host: "localhost",
+  database: "always music",
+  password: "Pauli1989+",
+  port: 5432,
+};
+
+const pool = new Pool(config);
+
+// Función para registrar un nuevo estudiante
+async function registrarEstudiante(nombre, rut, curso, nivel) {
+  try {
+    const query = 'INSERT INTO estudiantes(nombre, rut, curso, nivel) VALUES($1, $2, $3, $4)';
+    const values = [nombre, rut, curso, nivel];
+    const result = await pool.query(query, values);
+    console.log('Estudiante registrado correctamente.');
+    return result.rows;
+  } catch (error) {
+    console.error('Error al registrar estudiante:', error);
+  }
+}
+
+// Función para obtener un estudiante por su rut
+async function obtenerEstudiantePorRut(rut) {
+  try {
+    const query = 'SELECT * FROM estudiantes WHERE rut = $1';
+    const result = await pool.query(query, [rut]);
+    console.log(result.rows);
+    return result.rows;
+  } catch (error) {
+    console.error('Error al obtener estudiante por rut:', error);
+  }
+}
+
+// Función para obtener todos los estudiantes
+async function obtenerTodosLosEstudiantes() {
+  try {
+    const query = 'SELECT * FROM estudiantes';
+    const result = await pool.query(query);
+    console.log(result.rows);
+    return result.rows;
+  } catch (error) {
+    console.error('Error al obtener todos los estudiantes:', error);
+  }
+}
+
+// Función para actualizar los datos de un estudiante
+async function actualizarEstudiante(nombre, rut, curso, nivel) {
+  try {
+    const query = 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4';
+    const values = [nombre, curso, nivel, rut];
+    const result = await pool.query(query, values);
+    console.log('Estudiante actualizado correctamente.');
+    return result.rows;
+  } catch (error) {
+    console.error('Error al actualizar estudiante:', error);
+  }
+}
+
+// Función para eliminar un estudiante por su rut
+async function eliminarEstudiantePorRut(rut) {
+  try {
+    const query = 'DELETE FROM estudiantes WHERE rut = $1';
+    const result = await pool.query(query, [rut]);
+    console.log('Estudiante eliminado correctamente.');
+    return result.rows;
+  } catch (error) {
+    console.error('Error al eliminar estudiante:', error);
+  }
+}
+
+/*CONSULTAS
+registrarEstudiante('Camila Salas' '24576891-8' 'Guitarra' 'Principiante');
+obtenerEstudiantePorRut('34567890-1');
+obtenerTodosLosEstudiantes();
+actualizarEstudiante();
+eliminarEstudiantePorRut('24576891-8');
+*/
+
+
+
+
+
+
+
+
+
+
